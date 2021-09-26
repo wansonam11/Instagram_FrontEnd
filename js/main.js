543a27af71cbb74149814c200ccf9ade6df94355
전량원 <rise@jeonlyang-won-ui-MacBookPro.local>
2021-09-26 15:03:42 +0900

    frond end

---
+++ js/main.js
@@ -0,0 +1,217 @@
+
+const header = document.querySelector('#header');
+const sidebox = document.querySelector('.side_box');
+const variableWidth = document.querySelectorAll('.contents_box .contents');
+const delegation = document.querySelector(".contents_box");
+
+
+function delegationFunc(e){
+    let elem = e.target;
+    console.log(elem);
+
+    while (!elem.getAttribute('data-name')){
+        elem = elem.parentNode;
+
+        if(elem.nodeName === "BODY"){
+            elem = null;
+            return;
+        }
+    }
+
+    if(elem.matches('[data-name="heartbeat"]')){
+        console.log("하트!");
+        let pk = elem.getAttribute('name');
+
+        $.ajax({
+            type : "POST",
+            url : "dataFile/like.json",
+            data : {pk},
+            dataType : 'json',
+            success : function(response){
+                let likeCount = document.querySelector("#like-count-37");
+                likeCount.innerHTML = '좋아요' + response.like_count + '개';
+            },
+            error : function(request,status,error){
+                alert('로그인이 필요합니다.');
+                window.location.replace('http://www.naver.com');
+            },
+        })
+
+    }else if(elem.matches('[data-name="bookmark"]')){
+        console.log("북마크!");
+        let pk = elem.getAttribute('name');
+
+        $.ajax({
+            type : 'POST',
+            url : "dataFile/bookmark.json",
+            data : {pk},
+            success : function(response){
+                let bookmarkCount = document.querySelector('#bookmark-count-37');
+                bookmarkCount.innerHTML = '북마크' + response.bookmark_count + '개';
+            },
+            error : function(request,status,error){
+                alert('로그인이 필요합니다.');
+                window.location.replace('http://www.naver.com');
+            },
+        })
+
+    }else if(elem.matches('[data-name="comment"]')){
+        let content = document.querySelector('#add-comment-post37 > input[type=text]').value;
+        console.log(content);
+
+        if(content.length > 140){
+            alert("댓글은 최대 140자 입력 가능합니다. 현재 글자수 : " + content.length);
+            return;
+        }
+
+        $.ajax({
+            type : "POST",
+            url : "./comment.html",
+            data : {
+                'pk' : 37,
+                'content' : content,
+            },
+            dataType: 'html',
+            success : function (data){
+                document.querySelector("#comment-list-ajax-post37").insertAdjacentHTML('afterbegin', data);
+            },
+            error : function(request,status,error){
+                alert("문제가 발생했습니다.");
+            },
+        });
+
+        document.querySelector('#add-comment-post37 > input[type=text]').value = '';
+
+    }else if(elem.matches('[data-name="comment-delete"]')){
+       $.ajax({
+           type : 'POST',
+           url : 'dataFile/delete.json',
+           data : {
+               'pk' : 37,
+           },
+           dataType : 'json',
+           success : function(response){
+               if(response.status) {
+                   let comt = document.querySelector('.comment-detail');
+                   comt.remove();
+               }
+           },
+           error : function(request,status,error){
+               alert("문제가 발생했습니다.");
+           },
+       });
+
+    }else if(elem.matches('[data-name="follow"]')) {
+        $.ajax({
+            type : 'POST',
+            url : 'dataFile/follow.json',
+            data : {
+                'pk' : 37,
+            },
+            dataType : 'json',
+            success:function(response){
+                if(response.status) {
+                    document.querySelector('input.follow').value = "팔로잉";
+                }else{
+                    document.querySelector('input.follow').value = "팔로워";
+                }
+            },
+            error : function(request,status,error){
+                alert("문제가 발생했습니다.");
+            },
+        })
+
+    }
+    elem.classList.toggle('on');
+}
+
+
+function resizeFunc(){
+    console.log("resize!!");
+    if(pageYOffset >= 10){
+        let calcWidth = (window.innerWidth *0.5) + 167;
+        console.log(window.innerWidth *0.5);
+
+        sidebox.style.left = calcWidth + 'px';
+    }
+
+    if(matchMedia('screen and (max-width : 800px)').matches){
+       for(let i=0; i<variableWidth.length; i++){
+           variableWidth[i].style.width = window.innerWidth -20 + "px";
+       }
+    }else{
+        for(let i=0; i<variableWidth.length; i++) {
+            if(window.innerWidth > 600) {
+                variableWidth[i].removeAttribute("style");
+            }
+        }
+    }
+}
+
+
+function scrollFunc(){
+
+    let scrollHeight = pageYOffset + window.innerHeight;
+    let documentHeight = document.body.scrollHeight;
+
+    console.log('scrollHeight: ' + scrollHeight);
+    console.log('documentHeight: ' + documentHeight);
+
+    if(pageYOffset >= 10){
+        header.classList.add('on');
+
+        if(sidebox) {
+            sidebox.classList.add('on');
+        }
+        resizeFunc();
+    }else {
+        header.classList.remove('on');
+        if(sidebox) {
+            sidebox.classList.remove('on');
+            sidebox.removeAttribute('style');
+        }
+    }
+
+    if(scrollHeight >= documentHeight){
+        let page = document.querySelector('#page').value;
+        document.querySelector('#page').value = parseInt(page) + 1;
+
+        callMorePostAjax(page);
+
+        if(page > 4) {
+            return;
+        }
+    }
+}
+
+function callMorePostAjax(page){
+    if(page > 4) {
+        return;
+    }
+    $.ajax({
+        type : 'POST',
+        url : './post.html',
+        data : {
+            'page' : page,
+        },
+        dataType : 'html',
+        success : addMorePostAjax,
+        error : function(request,status,error){
+            alert("문제가 발생했습니다.");
+      },
+    })
+}
+
+function addMorePostAjax(data){
+    delegation.insertAdjacentHTML('beforeend', data);
+}
+
+setTimeout(function(){
+    scrollTo(0, 0)
+}, 100);
+
+if(delegation) {
+    delegation.addEventListener("click", delegationFunc);
+}
+window.addEventListener('resize', resizeFunc);
+window.addEventListener('scroll', scrollFunc);
